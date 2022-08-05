05e0056202533e1a6215407efca1fb26b3bd1244
Wojciech Ossowski <w.j.ossowski@gmail.com>
2022-08-05 13:57:16 +0200

    Changing tapping for Sym and Num layers

--- keymap.c
+++ keymap.c
@@ -34,7 +34,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [BASE] = LAYOUT(
-      MTM(TAB), KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,                                                         KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , MTS(TAB),
+      MTM(TAB), KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,                                                         KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , MTC(TAB),
       KC_CAPS , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    ,                                                         KC_H    , KC_J    , KC_K    , KC_L    , KC_QUOT , MTS(TAB),
       KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , XXXXXXX , XXXXXXX ,                 XXXXXXX , XXXXXXX , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , MTC(TAB),
                                     XXXXXXX , MTC(SPC), MTA(SPC), MTG(SPC), SPC_LSYM,                 ENT_LNUM,MTA(BSPC),MTA(BSPC),MTG(BSPC), XXXXXXX
@@ -67,10 +67,10 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case MTA(BSPC):
         case MTG(BSPC):
-        case SPC_LSYM:
+            return TAPPING_TERM - 100;
         case ENT_LNUM:
         case ENT_LFUN:
-            return TAPPING_TERM - 100;
+            return TAPPING_TERM - 50;
         default:
             return TAPPING_TERM;
     }
