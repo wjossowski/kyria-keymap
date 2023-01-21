b63c0374eba154328fa7f43361081e820b606039
Wojciech Ossowski <w.j.ossowski@gmail.com>
2023-01-21 13:34:51 +0100

    Added support for LED

--- keymap.c
+++ keymap.c
@@ -3,11 +3,11 @@
 #include "layers.h"
 
 #ifdef OLED_ENABLE
-#    include "oled.h"
+#include "oled.h"
 #endif
 
 #ifdef ENCODER_ENABLE
-#    include "encoder.h"
+#include "encoder.h"
 #endif
 
 // Mod-Tap
@@ -26,11 +26,11 @@
 #define ENT_FUN LT(FUN, KC_ENT)
 
 // Single Keys - just for less typing
-#define SPC     KC_SPC
-#define BSPC    KC_BSPC
-#define NA      XXXXXXX
-#define TAB     KC_TAB
-#define SCLN    KC_SCLN
+#define SPC KC_SPC
+#define BSPC KC_BSPC
+#define NA XXXXXXX
+#define TAB KC_TAB
+#define SCLN KC_SCLN
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -38,22 +38,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
       MTM(TAB), KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,                                                         KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    ,MTM(BSPC),
       KC_CAPS , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    ,                                                         KC_H    , KC_J    , KC_K    , KC_L    , KC_QUOT ,MTS(SCLN),
-      KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , XXXXXXX , XXXXXXX ,                 XXXXXXX , XXXXXXX , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT ,
-                                    KC_LCTL , KC_LALT , MTG(SPC), SPC_SYM , ENT_SYM ,                 ENT_NUM , BSPC_NUM, KC_LALT , KC_LGUI , KC_LCTL
+      KC_LCTL , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , XXXXXXX , XXXXXXX ,                 XXXXXXX , XXXXXXX , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT ,
+                                    KC_LCTL , KC_LALT , MTG(SPC), MTS(SPC), SPC_SYM ,                BSPC_NUM ,MTA(BSPC), KC_LALT , KC_LGUI , KC_LCTL
     ),
 
     [SYM] = LAYOUT(
       KC_0    , KC_CIRC , KC_PERC , KC_HASH , KC_PLUS , KC_UNDS ,                                                         KC_MINUS, KC_EQL  , KC_ASTR , KC_PERC , KC_DLR  , KC_DLR  ,
       KC_COMM , KC_PERC , KC_LABK , KC_LBRC , KC_LCBR , KC_LPRN ,                                                         KC_COLN , KC_PIPE , KC_QUES,  KC_DQUO , KC_GRV  , KC_SCLN ,
       _______ , XXXXXXX , KC_RABK , KC_RBRC , KC_RCBR , KC_RPRN , XXXXXXX , XXXXXXX ,                 XXXXXXX , XXXXXXX , KC_SCLN , KC_BSLS , KC_EXLM,  KC_QUOTE, KC_BSLS , _______ ,
-                                    _______ , _______ , _______ , _______ , _______ ,                 MO(FUN) , MO(FUN) , _______ , _______ , _______
+                                    _______ , _______ , _______ , _______ , _______ ,                 MO(FUN) , ENT_FUN , _______ , _______ , _______
     ),
 
     [NUM] = LAYOUT(
       KC_TILDE, KC_LPRN , KC_1    , KC_2    , KC_3    , KC_COMM ,                                                         MTM(NA) , MTM(NA) , MTM(NA) , MTM(NA) , MTM(NA) , _______ ,
       _______ , KC_RPRN , KC_4    , KC_5    , KC_6    , KC_DOT  ,                                                         KC_EQL  , KC_PLUS , KC_ASTR , XXXXXXX , XXXXXXX , _______ ,
       _______ , KC_0    , KC_7    , KC_8    , KC_9    , KC_0    , XXXXXXX , XXXXXXX ,                 XXXXXXX , XXXXXXX , KC_EQL  , KC_MINUS, KC_SLSH , XXXXXXX , XXXXXXX , _______ ,
-                                    _______ , _______ , _______ , MO(FUN) , MO(FUN) ,                 _______ , _______ , _______ , _______ , _______
+                                    _______ , _______ , _______ , ENT_FUN , MO(FUN) ,                 _______ , _______ , _______ , _______ , _______
     ),
 
     [FUN] = LAYOUT(
