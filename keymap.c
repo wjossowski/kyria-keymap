65e2128a19b7789e975f82e90af035e2ab23e0a7
Wojciech Ossowski <w.j.ossowski@gmail.com>
2023-04-20 15:02:43 +0200

    Moved backspace under layer key

--- keymap.c
+++ keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MTM(TAB), KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,                                                         KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , MTM(KC_BSPC),
     KC_CAPS , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    ,                                                         KC_H    , KC_J    , KC_K    , KC_L    , KC_QUOT , MTS(SCLN),
     MTC(LBR), KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , XXXXXXX , XXXXXXX ,                 XXXXXXX , XXXXXXX , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , MTC(KC_RBRC),
-                                  KC_LPRN , KC_LALT , MTG(SPC), MTS(SPC), SPC_SYM ,                 MO(NUM) ,MTS(BSPC), KC_LALT , KC_LGUI , KC_RPRN
+                                  KC_LPRN , KC_LALT , MTG(SPC), MTS(SPC), SPC_SYM ,                 BSP_NUM , KC_LSFT , KC_LALT , KC_LGUI , KC_RPRN
   ),
 
   [SYM] = LAYOUT(
