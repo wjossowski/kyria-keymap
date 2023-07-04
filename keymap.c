559e4cb6b9a92576f6563b9efd69fe75c9e47f00
Wojciech Ossowski <w.j.ossowski@gmail.com>
2023-07-04 11:47:54 +0200

    autoshift pt2

--- keymap.c
+++ keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MTM(TAB), KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,                                                         KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , MTM(LBR),
     KC_CAPS , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    ,                                                         KC_H    , KC_J    , KC_K    , KC_L    , KC_QUOT , MTS(SCLN),
     MTC(LBR), KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , XXXXXXX , XXXXXXX ,                 XXXXXXX , XXXXXXX , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , MTG(RBR),
-                                  XXXXXXX , KC_LALT , KC_LGUI , MTS(SPC), MO(SYM) ,                 MO(NUM) ,MTS(BSPC), KC_LALT , KC_LGUI , XXXXXXX
+                                  XXXXXXX , XXXXXXX , KC_LGUI , MTA(SPC), MO(SYM) ,                 MO(NUM) ,MTA(BSPC), KC_LGUI , XXXXXXX , XXXXXXX
   ),
 
   [SYM] = LAYOUT(
